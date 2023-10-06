171afbf67b5bacfa5c3620ab528810227b35b9d1
Tom Rayner <tomrayware@gmail.com>
2023-10-06 20:02:30 +0100

    Main function and more

--- Source/Engine/Core/Engine.h
+++ Source/Engine/Core/Engine.h
@@ -1,9 +1,21 @@
 #pragma once
 
+#include "Logger.h"
+
+inline Logger* logger = new Logger();
+
+inline RWEngine::BaseApplication* g_pApp; // defined in entry point
+
 // RWEngine namespace for internal use,
 // Engine namespace for external use!
 
 namespace RWEngine {
+}
 
+#include "Application/BaseApplication.h"
+
+namespace Engine {
+
+	extern RWEngine::BaseApplication* GetApplication();
 
 }
---
+++ Source/Engine/Core/EntryPoint.cpp
@@ -0,0 +1,43 @@
+#include "StdAfx.h"
+
+#include "Engine.h"
+
+#include <DetectPlatform.h>
+
+// Hide the command prompt pop-up in release mode
+#if IsWin()
+# 	ifndef _DEBUG
+#		pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
+#	endif
+#endif
+
+void SmallCleanup();
+
+int main(int argc, char* argv)
+{
+	g_pApp = Engine::GetApplication();
+
+	if (g_pApp == nullptr)
+	{
+		logger->Critical("Engine::GetApplication returned nullptr, define in client !");
+		SmallCleanup();
+		return 0;
+	}
+
+	SmallCleanup();
+	return 0;
+}
+
+//
+// Must happen before the program closes!
+// Purpose: Free parts of memory that are allocated before closing.
+//
+void SmallCleanup()
+{
+	if (g_pApp != nullptr)
+	{
+		delete g_pApp;
+	}
+	
+	delete logger;
+}
