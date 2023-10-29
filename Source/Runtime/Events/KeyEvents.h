663601e356139f3c4db4f25f3801eeccebd2f348
Tom Rayner <tomrayware@gmail.com>
2023-10-29 13:54:53 +0000

    runtime: refactor and add keyup event

--- Source/Runtime/Events/KeyEvents.h
+++ Source/Runtime/Events/KeyEvents.h
@@ -29,4 +29,29 @@ namespace Engine {
 		int m_KeyID;
 	};
 
+	class KeyUpEvent : public Event
+	{
+	public:
+		KeyUpEvent(int key)
+			: m_KeyID(key)
+		{
+		}
+
+		virtual EventType GetType() const override
+		{
+			return
+			{
+				"KeyUp", "Key"
+			};
+		}
+
+		inline int GetKeyID() const
+		{
+			return m_KeyID;
+		}
+
+	private:
+		int m_KeyID;
+	};
+
 }
