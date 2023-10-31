183984b7475151834749488ca0c613af0f217fff
Tom Rayner <tomrayware@gmail.com>
2023-10-31 01:23:50 +0000

    runtime: remove dispatcher

--- Source/Runtime/Events/EventDispatcher.h
+++ Source/Runtime/Events/EventDispatcher.h
@@ -6,18 +6,6 @@
 
 namespace Engine {
 
-	class EventDispatcher
-	{
-		template<typename T>
-		using EventFunc = std::function<bool(T&)>;
-	public:
-		EventDispatcher(Event& event);
-
-		template<typename T>
-		bool Dispatch(EventFunc<T> func);
-
-	private:
-		Event& m_Event;
-	};
+	class EventDispatcher;
 
 }
